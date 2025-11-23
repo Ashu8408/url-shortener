aa0875f2174707d3f1e2d5054f77b81d0619d8da
ashuOO7 <ashjnavj@gmail.com>
2025-11-23 15:31:19 +0530

    setting routes

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const URL = require("../models/url");
 const { handleGenerateNewShortURL, handleGetAnalytics, handleDeleteURL } = require("../controllers/url");
 
 const router = express.Router();
@@ -6,5 +7,10 @@ const router = express.Router();
 router.post("/", handleGenerateNewShortURL);
 router.get("/analytics/:shortId", handleGetAnalytics);
 router.delete("/:shortId", handleDeleteURL);
+
+router.get("/all", async (req, res) => {
+    const all = await URL.find({});
+    res.json(all);
+});
 
 module.exports = router;
